b31619a9b9906253f4d21fcefe6bda93730e0fdb
Kooreis <kooreis.official@gmail.com>
2024-12-29 21:57:39 -0600

    This update introduces the get_skyline function. This function takes a list of buildings as input, and creates a list of BuildingPoint instances representing the start and end points of each building. The list of BuildingPoint instances is then sorted.

--- solution.py
+++ solution.py
@@ -1,21 +1,7 @@
-class BuildingPoint(object):
-    def __init__(self, point, is_start, height):
-        self.point = point
-        self.is_start = is_start
-        self.height = height
+def get_skyline(buildings):
+    building_points = []
+    for building in buildings:
+        building_points.append(BuildingPoint(building[0], True, building[2]))
+        building_points.append(BuildingPoint(building[1], False, building[2]))
 
-    def __lt__(self, other):
-        if self.point != other.point:
-            return self.point < other.point
-        else:
-            if self.is_start:
-                h1 = -self.height
-            else:
-                h1 = self.height
-
-            if other.is_start:
-                h2 = -other.height
-            else:
-                h2 = other.height
-
-            return h1 < h2
+    building_points.sort()
