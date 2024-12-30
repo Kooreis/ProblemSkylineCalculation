55b8d28137873355b2d49817c38ac939f4bbe317
Kooreis <kooreis.official@gmail.com>
2024-12-29 21:57:39 -0600

    This is the start of the getSkyline function. It initializes an empty result list and a height list. It then iterates over the buildings, adding two entries to the height list for each building: one for the left edge and one for the right edge.

--- solution.java
+++ solution.java
@@ -1,10 +1,7 @@
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        int[][] buildings = {{2, 9, 10}, {3, 7, 15}, {5, 12, 12}, {15, 20, 10}, {19, 24, 8}};
-        List<int[]> result = getSkyline(buildings);
-        for (int[] point : result) {
-            System.out.println(Arrays.toString(point));
+public static List<int[]> getSkyline(int[][] buildings) {
+        List<int[]> result = new ArrayList<>();
+        List<int[]> height = new ArrayList<>();
+        for (int[] b : buildings) {
+            height.add(new int[]{b[0], -b[2]});
+            height.add(new int[]{b[1], b[2]});
         }
-    }
